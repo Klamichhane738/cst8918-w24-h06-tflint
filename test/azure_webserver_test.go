28d2f4e9940c0e4000f2cd650c2f3266739dbfa0
Klamichhane738.Keshav <lami0053@algonquinlive.com>
2025-02-18 12:13:59 -0500

    Apply TFLint fixes and configure strict rules

--- test/azure_webserver_test.go
+++ test/azure_webserver_test.go
@@ -10,7 +10,7 @@ import (
 
 // You normally want to run this under a separate "Testing" subscription
 // For lab purposes you will use your assigned subscription under the Cloud Dev/Ops program tenant
-var subscriptionID string = "<your-subscription-id>"
+var subscriptionID string = "b296b604-9c48-4e98-bb66-56c661c39a1d"
 
 func TestAzureLinuxVMCreation(t *testing.T) {
 	terraformOptions := &terraform.Options{
@@ -18,7 +18,7 @@ func TestAzureLinuxVMCreation(t *testing.T) {
 		TerraformDir: "../",
 		// Override the default terraform variables
 		Vars: map[string]interface{}{
-			"labelPrefix": "<your-college-username>",
+			"label_prefix": "lami0053",
 		},
 	}
 
